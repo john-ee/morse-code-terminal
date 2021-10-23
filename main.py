6fb1d570a8f6df0831dcbc3e1a453a3a39ab0119
John-Nathan Hill <john.nat.hill@gmail.com>
2021-10-23 16:19:54 +0200

    Successfully send letters from on terminal to another

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+radio.set_group(1)
+
 MORSE_CODE = [
     '.-',
     '-...',
@@ -54,41 +56,70 @@ MORSE_DECODE = [
     'Y',
     'Z',
 ]
+ALPHABET = 26
+
+def on_gesture_shake():
+    global code
+    global word
+    code = ""
+    for i in range(len(word)):
+        letter_decoder(word[i])
+        radio.send_string(word[i])
+    word = []
+    basic.clear_screen()
+
+def on_received_code(receivedString):
+    letter_decoder(receivedString)
+    basic.clear_screen()
+
+
+def letter_decoder(code):
+    indent = -1
+    for i in range(0,ALPHABET):
+        if code == MORSE_CODE[i]:
+            indent = i
+    if indent > -1:
+        basic.show_string(MORSE_DECODE[indent])
+        return 1
+    else:
+        basic.show_leds("""
+                . . . . .
+                . # . # .
+                . . # . .
+                . # . # .
+                . . . . .
+            """)
+        return 0
 
 MAX = 30
-ALPHABET = 26
+IDLE = 10
 code = ""
+word = [""]
+word = []
 def on_forever():
-    counting = 0
     global code
+    global word
+    counting = 0
+    spacestart = control.millis()
     while input.button_is_pressed(Button.A):
         basic.clear_screen()
-        basic.pause(10)
+        basic.pause(IDLE)
         counting+=1
-    if counting > 1 and counting < MAX:
+    if counting > 0 and counting < MAX:
         led.plot(0, 0)#.
         code = "" + code + "."
     elif counting >= MAX:
         led.plot(0, 0)
         led.plot(1, 0)#-
-        code = "" + code + "-"
+        code = "" + code + "-"   
     if input.button_is_pressed(Button.B):
-        indent = -1
-        for i in range(ALPHABET):
-            if code == MORSE_CODE[i]:
-                indent = i
-        if indent > -1:
-            basic.show_string(MORSE_DECODE[indent])
-        else:
-            basic.show_leds("""
-                    . . . . .
-                    . # . # .
-                    . . # . .
-                    . # . # .
-                    . . . . .
-                """)
+        decoded = letter_decoder(code)
+        if decoded == 1:
+            word.append(code)
+        #basic.show_number(len(word))
         basic.clear_screen()
         code = ""
     basic.pause(10)
+radio.on_received_string(on_received_code)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 basic.forever(on_forever)
-
--- main.ts
+++ main.ts
@@ -1,18 +1,63 @@
+radio.setGroup(1)
 let MORSE_CODE = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
 let MORSE_DECODE = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-let MAX = 30
 let ALPHABET = 26
-let code = ""
-basic.forever(function on_forever() {
-    let indent: number;
-    let counting = 0
+function letter_decoder(code: string): number {
+    let indent = -1
+    for (let i = 0; i < ALPHABET; i++) {
+        if (code == MORSE_CODE[i]) {
+            indent = i
+        }
+        
+    }
+    if (indent > -1) {
+        basic.showString(MORSE_DECODE[indent])
+        return 1
+    } else {
+        basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . # . .
+                . # . # .
+                . . . . .
+            `)
+        return 0
+    }
     
+}
+
+let MAX = 30
+let IDLE = 10
+let code = ""
+let word = [""]
+word = []
+radio.onReceivedString(function on_received_code(receivedString: string) {
+    letter_decoder(receivedString)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    
+    
+    code = ""
+    for (let i = 0; i < word.length; i++) {
+        letter_decoder(word[i])
+        radio.sendString(word[i])
+    }
+    word = []
+    basic.clearScreen()
+})
+basic.forever(function on_forever() {
+    let decoded: number;
+    
+    
+    let counting = 0
+    let spacestart = control.millis()
     while (input.buttonIsPressed(Button.A)) {
         basic.clearScreen()
-        basic.pause(10)
+        basic.pause(IDLE)
         counting += 1
     }
-    if (counting > 1 && counting < MAX) {
+    if (counting > 0 && counting < MAX) {
         led.plot(0, 0)
         // .
         code = "" + code + "."
@@ -24,25 +69,12 @@ basic.forever(function on_forever() {
     }
     
     if (input.buttonIsPressed(Button.B)) {
-        indent = -1
-        for (let i = 0; i < ALPHABET; i++) {
-            if (code == MORSE_CODE[i]) {
-                indent = i
-            }
-            
-        }
-        if (indent > -1) {
-            basic.showString(MORSE_DECODE[indent])
-        } else {
-            basic.showLeds(`
-                    . . . . .
-                    . # . # .
-                    . . # . .
-                    . # . # .
-                    . . . . .
-                `)
+        decoded = letter_decoder(code)
+        if (decoded == 1) {
+            word.push(code)
         }
         
+        // basic.show_number(len(word))
         basic.clearScreen()
         code = ""
     }

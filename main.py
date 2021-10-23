c62d104cbad08635b4e70a128007af42dc18f6b1
John-Nathan Hill <john.nat.hill@gmail.com>
2021-10-23 15:08:24 +0200

    functional device

--- main.py
+++ main.py
@@ -1,74 +1,94 @@
-MORSE_CODE = {
-    '.-': 'A',
-    '-...':'B',
-    '-.-.':'C',
-    '-..': 'D',
-    '.': 'E',
-    '..-.': 'F',
-    '--.': 'G',
-    '....': 'H',
-    '..': 'I',
-    '.---': 'J',
-    '-.-': 'K',
-    '.-..': 'L',
-    '--': 'M',
-    '-.': 'N',
-    '---': 'O',
-    '.--.': 'P',
-    '--.-': 'Q',
-    '.-.': 'R',
-    '...': 'S',
-    '-': 'T',
-    '..-': 'U',
-    '...-': 'V',
-    '.--': 'W',
-    '-..-': 'X',
-    '-.--': 'Y',
-    '--..': 'Z'
-}
-MORSE_DECODE = {
-    'A': '.-',
-    'B': '-...',
-    'C': '-.-.',
-    'D': '-..',
-    'E': '.',
-    'F': '..-.',
-    'G': '--.',
-    'H': '....',
-    'I': '..',
-    'J': '.---',
-    'K': '-.-',
-    'L': '.-..',
-    'M': '--',
-    'N': '-.',
-    'O': '---',
-    'P': '.--.',
-    'Q': '--.-',
-    'R': '.-.',
-    'S': '...',
-    'T': '-',
-    'U': '..-',
-    'V': '...-',
-    'W': '.--',
-    'X': '-..-',
-    'Y': '-.--',
-    'Z': '--..'
-}
-MAX = 15
+MORSE_CODE = [
+    '.-',
+    '-...',
+    '-.-.',
+    '-..',
+    '.',
+    '..-.',
+    '--.',
+    '....',
+    '..',
+    '.---',
+    '-.-',
+    '.-..',
+    '--',
+    '-.',
+    '---',
+    '.--.',
+    '--.-',
+    '.-.',
+    '...',
+    '-',
+    '..-',
+    '...-',
+    '.--',
+    '-..-',
+    '-.--',
+    '--..',
+]
+MORSE_DECODE = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+]
 
+MAX = 30
+ALPHABET = 26
+code = ""
 def on_forever():
     counting = 0
+    global code
     while input.button_is_pressed(Button.A):
         basic.clear_screen()
         basic.pause(10)
         counting+=1
     if counting > 1 and counting < MAX:
-        led.plot(0, 0)
-        #.
+        led.plot(0, 0)#.
+        code = "" + code + "."
     elif counting >= MAX:
         led.plot(0, 0)
-        led.plot(1, 0)
-        #-
-    #if input.button_is_pressed(Button.B):
+        led.plot(1, 0)#-
+        code = "" + code + "-"
+    if input.button_is_pressed(Button.B):
+        indent = -1
+        for i in range(ALPHABET):
+            if code == MORSE_CODE[i]:
+                indent = i
+        if indent > -1:
+            basic.show_string(MORSE_DECODE[indent])
+        else:
+            basic.show_leds("""
+                    . . . . .
+                    . # . # .
+                    . . # . .
+                    . # . # .
+                    . . . . .
+                """)
+        basic.clear_screen()
+        code = ""
     basic.pause(10)
 basic.forever(on_forever)
+
--- main.ts
+++ main.ts
@@ -1,64 +1,12 @@
-let MORSE_CODE = {
-    ".-" : "A",
-    "-..." : "B",
-    "-.-." : "C",
-    "-.." : "D",
-    "." : "E",
-    "..-." : "F",
-    "--." : "G",
-    "...." : "H",
-    ".." : "I",
-    ".---" : "J",
-    "-.-" : "K",
-    ".-.." : "L",
-    "--" : "M",
-    "-." : "N",
-    "---" : "O",
-    ".--." : "P",
-    "--.-" : "Q",
-    ".-." : "R",
-    "..." : "S",
-    "-" : "T",
-    "..-" : "U",
-    "...-" : "V",
-    ".--" : "W",
-    "-..-" : "X",
-    "-.--" : "Y",
-    "--.." : "Z",
-}
-
-let MORSE_DECODE = {
-    "A" : ".-",
-    "B" : "-...",
-    "C" : "-.-.",
-    "D" : "-..",
-    "E" : ".",
-    "F" : "..-.",
-    "G" : "--.",
-    "H" : "....",
-    "I" : "..",
-    "J" : ".---",
-    "K" : "-.-",
-    "L" : ".-..",
-    "M" : "--",
-    "N" : "-.",
-    "O" : "---",
-    "P" : ".--.",
-    "Q" : "--.-",
-    "R" : ".-.",
-    "S" : "...",
-    "T" : "-",
-    "U" : "..-",
-    "V" : "...-",
-    "W" : ".--",
-    "X" : "-..-",
-    "Y" : "-.--",
-    "Z" : "--..",
-}
-
-let MAX = 15
+let MORSE_CODE = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
+let MORSE_DECODE = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+let MAX = 30
+let ALPHABET = 26
+let code = ""
 basic.forever(function on_forever() {
+    let indent: number;
     let counting = 0
+    
     while (input.buttonIsPressed(Button.A)) {
         basic.clearScreen()
         basic.pause(10)
@@ -66,13 +14,38 @@ basic.forever(function on_forever() {
     }
     if (counting > 1 && counting < MAX) {
         led.plot(0, 0)
-    } else if (counting >= MAX) {
         // .
+        code = "" + code + "."
+    } else if (counting >= MAX) {
         led.plot(0, 0)
         led.plot(1, 0)
+        // -
+        code = "" + code + "-"
     }
     
-    // -
-    // if input.button_is_pressed(Button.B):
+    if (input.buttonIsPressed(Button.B)) {
+        indent = -1
+        for (let i = 0; i < ALPHABET; i++) {
+            if (code == MORSE_CODE[i]) {
+                indent = i
+            }
+            
+        }
+        if (indent > -1) {
+            basic.showString(MORSE_DECODE[indent])
+        } else {
+            basic.showLeds(`
+                    . . . . .
+                    . # . # .
+                    . . # . .
+                    . # . # .
+                    . . . . .
+                `)
+        }
+        
+        basic.clearScreen()
+        code = ""
+    }
+    
     basic.pause(10)
 })
